ea00e31988ec54891d8b60d659bbeac8f87381d5
osamagucCitySoc <osama.cs.eng@gmail.com>
2014-05-01 09:33:31 +0400

    Initial Commit

---
+++ emsahWerbah/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  emsahWerbah
+//
+//  Created by OsamaMac on 5/1/14.
+//  Copyright (c) 2014 OsamaMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ emsahWerbah/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  emsahWerbah
+//
+//  Created by OsamaMac on 5/1/14.
+//  Copyright (c) 2014 OsamaMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
